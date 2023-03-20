b7e6e6544ec5ee611e46d62f13a2a385c89c7acf
vamshikrishnadn <vamshikrishnadn573@gmail.com>
2023-03-20 09:13:18 +0530

    package changes

--- frontend/src/services/AppServices.js
+++ frontend/src/services/AppServices.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const endPoint = 'http://localhost:8080';
+const endPoint = 'http://3.109.62.86:8080';
+// const endPoint = 'http://localhost:8080';
 
 export const addOrder = formValues => axios.post(`${endPoint}/orders/create`, formValues);
 export const getOrder = () => axios.get(`${endPoint}/orders/getall`);
